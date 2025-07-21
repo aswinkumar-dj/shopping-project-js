82f5560dd8dffedef8e4634f41da54f72f12f290
aswinkumar-dj <203110173+aswinkumar-dj@users.noreply.github.com>
2025-07-22 01:57:23 +0530

    Cleaned the code

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,19 @@
 export const cart =[];
+
+export function addToCart(productId){
+    let matchingItem;
+            cart.forEach((cartItem)=>{
+                if(productId===cartItem.matchingItem){
+                    matchingItem = item;
+                }
+            });
+
+            if(matchingItem){
+                matchingItem += 1;
+            } else{
+                cart.push({
+                    productId: productId,
+                    quantity: 1,
+                });
+            };
+};
